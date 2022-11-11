e47840c4b4f288ac58c2f47a4c993fa4c3c23d9a
Anurag-1427 <kaushikanurag2001@gmail.com>
2022-11-11 15:11:48 +0530

    authentication done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//Routing
+import PrivateRoute from "./components/routing/PrivateRoute";
+
+// Screens
+import PrivateScreen from "./components/screens/PrivateScreen";
+import LoginScreen from "./components/screens/LoginScreen";
+import RegisterScreen from "./components/screens/RegisterScreen";
+import ForgotPasswordScreen from "./components/screens/ForgotPasswordScreen";
+import ResetPasswordScreen from "./components/screens/ResetPasswordScreen";
 
 const App = () => {
-  return <div className="App">React App</div>;
+  return (
+    <>
+      <Router>
+        <div className="app">
+          <Switch>
+            <PrivateRoute exact path="/" component={PrivateScreen} />
+            <Route exact path="/login" component={LoginScreen} />
+            <Route exact path="/register" component={RegisterScreen} />
+            <Route
+              exact
+              path="/forgotpassword"
+              component={ForgotPasswordScreen}
+            />
+            <Route
+              exact
+              path="/resetpassword/:resetToken"
+              component={ResetPasswordScreen}
+            />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
 };
 
 export default App;
