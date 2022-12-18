85f9981e0f98fcaacc16c3831e05e262d8bdf442
Anurag-1427 <kaushikanurag2001@gmail.com>
2022-12-18 16:37:11 +0530

    weather part done (prev commit is of yesterday)

--- client/src/components/screens/PrivateScreen.js
+++ client/src/components/screens/PrivateScreen.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Navbar from "./Navbar";
+import "./PrivateScreen.css";
+import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
 
 const PrivateScreen = ({ history }) => {
   const [error, setError] = useState("");
   const [privateData, setPrivateData] = useState("");
+
+  // These states for quotes part of DOM
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
 
   useEffect(() => {
     if (!localStorage.getItem("authToken")) {
@@ -36,14 +42,37 @@ const PrivateScreen = ({ history }) => {
     history.push("/login");
   };
 
+  // and this is the part of api calling for quotes
+  const fetchQuote = async () => {
+    const urlForQuotes = "http://api.quotable.io/random";
+    // const urlForQuotes = "https://type.fit/api/quotes";
+    const response = await axios.get(urlForQuotes);
+    console.log(response);
+    setQuote(response.data.content);
+    setAuthor(response.data.author);
+  };
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return error ? (
     <span className="error-message">{error}</span>
   ) : (
     <>
-      <Navbar />
-      <div style={{ background: "green", color: "white" }}>{privateData}</div>
-      <p>Here will be the main page task pages</p>
-      <button onClick={logoutHandler}>Logout</button>
+      <div className="container">
+        <Navbar />
+        <div style={{ background: "green", color: "white" }}>{privateData}</div>
+        <p>Here will be the main page task pages</p>
+        <button onClick={logoutHandler}>Logout</button>
+        <div className="quotes_container">
+          <div className="quote">
+            <FormatQuoteIcon />
+            {quote}
+            <FormatQuoteIcon />
+          </div>
+          <div className="author"> - {author}</div>
+        </div>
+      </div>
     </>
   );
 };
